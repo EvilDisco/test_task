2d19ef7d531f409af370ea5ca45abf950b671a69
FragHimself <madinhabitant@gmail.com>
2021-01-27 21:43:04 +0800

    Adding fixture for main entities

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,31 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Image;
+use App\Entity\File;
+use App\DBAL\EnumImageCategoryType;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $file = new File();
+            $file->setName('file '.$i);
+            $file->setPath('some_path_to_image_file.png');
+            $manager->persist($file);
+
+            $img = new Image();
+            $img->setName('image '.$i);
+            $img->setDescription('there goes description of image '.$i);
+
+            $imgCategories = [ EnumImageCategoryType::CATEGORY_NEW, EnumImageCategoryType::CATEGORY_POPULAR ];
+            $img->setCategory($imgCategories[ array_rand($imgCategories) ]);
+
+            $img->setFile($file);
+
+            $manager->persist($img);
+        }
 
         $manager->flush();
     }
