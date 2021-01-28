806e046d14f6b2f4579720b63487ca5e8a8506db
FragHimself <madinhabitant@gmail.com>
2021-01-29 07:02:31 +0800

    Changing File entity to match Api Platform upload requirements

--- src/Entity/File.php
+++ src/Entity/File.php
@@ -2,11 +2,52 @@
 
 namespace App\Entity;
 
+use App\Controller\CreateFileAction;
 use App\Repository\FileRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=FileRepository::class)
+ * @ApiResource(
+ *     iri="http://schema.org/MediaObject",
+ *     normalizationContext={
+ *         "groups"={"file_read"}
+ *     },
+ *     collectionOperations={
+ *         "post"={
+ *             "controller"=CreateFileAction::class,
+ *             "deserialize"=false,
+ *             "security"="is_granted('ROLE_USER')",
+ *             "validation_groups"={"Default", "file_create"},
+ *             "openapi_context"={
+ *                 "requestBody"={
+ *                     "content"={
+ *                         "multipart/form-data"={
+ *                             "schema"={
+ *                                 "type"="object",
+ *                                 "properties"={
+ *                                     "file"={
+ *                                         "type"="string",
+ *                                         "format"="binary"
+ *                                     }
+ *                                 }
+ *                             }
+ *                         }
+ *                     }
+ *                 }
+ *             }
+ *         },
+ *         "get"
+ *     },
+ *     itemOperations={
+ *         "get"
+ *     }
+ * )
+ * @Vich\Uploadable
  */
 class File
 {
@@ -18,41 +59,25 @@ class File
     private $id;
 
     /**
+     * @Vich\UploadableField(mapping="image", fileNameProperty="name")
+     */
+    public $file;
+
+    /**
      * @ORM\Column(type="string", length=255)
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @ApiProperty(iri="http://schema.org/contentUrl")
+     * @Groups({"file_read"})
      */
-    private $path;
+    public $path;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getPath(): ?string
-    {
-        return $this->path;
-    }
-
-    public function setPath(string $path): self
-    {
-        $this->path = $path;
-
-        return $this;
     }
 }
