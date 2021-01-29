7aff0ced150c5c3255745dbd8e0b9e4772877d90
FragHimself <madinhabitant@gmail.com>
2021-01-29 09:01:36 +0800

    Adding User entity to fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,15 +4,32 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
 use App\Entity\Image;
 use App\Entity\File;
 use App\DBAL\EnumImageCategoryType;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setEmail('user'.$i.'@example.com');
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'the_new_password'
+            ));
+            $manager->persist($user);
+
             $file = new File();
             $file->setName('file '.$i);
             $file->setPath('some_path_to_image_file.png');
