81cf9536de4359094b3b260ed8d097c2ba94eedd
FragHimself <madinhabitant@gmail.com>
2021-02-02 16:08:47 +0800

    Adding authenticated user to created Image entity via event subscriber

---
+++ src/EventSubscriber/AddOwnerToImageSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use App\Entity\Image;
+use App\Entity\User;
+
+final class AddOwnerToArticleSubscriber implements EventSubscriberInterface
+{
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['attachOwner', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function attachOwner(ViewEvent $event)
+    {
+        $image = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$image instanceof Image || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $token = $this->tokenStorage->getToken();
+        if (!$token) {
+            return;
+        }
+
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $image->setUser($user);
+    }
+}
